3be5d5c254c8dc5ace34d5a56aa1f20baf395b20
Tejas Gautam <tejasgautam004@gmail.com>
2026-05-31 10:49:52 +0530

    Improve GuessTheNumber game

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -5,11 +5,12 @@ public class GuessTheNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int myNum = (int)(Math.random()*100);
+        int myNum = (int)(Math.random() * 100) + 1;
         int userNumber = 0;
         
         do { 
-            System.out.println("Guess my number: ");
+            System.out.println("Guess a number between 1 and 100");
+            System.out.println("Enter -1 to quit");
             userNumber = sc.nextInt();
             if (userNumber == myNum) {
                 System.out.println("correct");
