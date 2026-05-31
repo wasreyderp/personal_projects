ab92b10e15e6e2b4f7bbcf1e17edc8227b74ed28
Tejas Gautam <tejasgautam004@gmail.com>
2026-05-31 10:41:55 +0530

    Remove tryncatch practice file

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class miniproject {
+public class GuessTheNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
